29efbed870c70da369b6566d821d5e3039c02765
Aasif Ahmed <asifarslaan@gmail.com>
2024-10-23 10:09:23 +0530

    start
    start

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<div id="section-not-to-print">
+    <footer class="content-footer footer bg-footer-theme mt-4">
+        <div class="container-fluid d-flex flex-wrap justify-content-between flex-md-row flex-column">
+            <div class="mb-md-0 d-flex align-items-start justify-content-between">
+                ©
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                , <?= $general_settings['footer_text'] ?>
+
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- / Footer -->
